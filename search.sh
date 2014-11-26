711cb59ad277a65bd4e5f29964d7de52bacd364b
LEdoian <tap9701@gmail.com>
2014-11-26 22:31:35 +0100

    Initial commit
    Currently, there is only a README.md and few lines of comments in the shell script

---
+++ search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A program to make search of your want in your favourite web browser
+
+#Usage:	./whatever <search term>		Perform a search on <search term>
+#	./whatever				Interactive mode (you'll be asked for your search term and for search engine, if needed)
+#	./whatever -a|--add <tag> <url>		Add a search engine to database
+#	./whatever -b|--browser <browser>	Use <browser> instead of default browser
+#	./whatever -r|--remove <tag>		Remove <tag> from database
+#	./whatever -d|--database <file>		Use other database file
+#	./whatever -v|--verbose			Verbose output - debugging stuff (Use this for bug reporting)
+
+
